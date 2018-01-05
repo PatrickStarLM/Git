3226ac36514d7d7872be220637c01a5114f2d8a2
LM <208621843@qq.com>
2018-01-05 21:24:33 +0800

    565656

--- src/com/zhiyou100/git/Test01.java
+++ src/com/zhiyou100/git/Test01.java
@@ -14,22 +14,9 @@ public class Test01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
+
+  
+
 	}
 
 }
